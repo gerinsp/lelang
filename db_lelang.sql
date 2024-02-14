db331d0153734eb2d850dc411b07c5e2d45c8b55
anggakp007 <putraangga2810@gmail.com>
2024-02-14 17:39:54 +0700

    Database Lelang

---
+++ db_lelang.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Feb 2024 pada 09.07
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_lelang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `id_customer` int(11) NOT NULL,
+  `nik` varchar(250) NOT NULL,
+  `nama_customer` varchar(250) NOT NULL,
+  `jenis_kelamin` varchar(50) NOT NULL,
+  `alamat` varchar(500) NOT NULL,
+  `no_hp` int(11) NOT NULL,
+  `id_sales` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL COMMENT 'Pendanda untuk melihat cara daftarnya'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` int(11) NOT NULL,
+  `status_show` int(11) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `create_by` int(11) NOT NULL,
+  `update_date` datetime NOT NULL,
+  `update_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pengajuanharga`
+--
+
+CREATE TABLE `tbl_pengajuanharga` (
+  `id_pengajuanharga` int(11) NOT NULL,
+  `tanggal_pengajuan` datetime NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `id_sales` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status_approve` int(11) NOT NULL COMMENT 'Untuk penanda pengajuan harga tampil di landing page'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_produk` varchar(250) NOT NULL,
+  `deskripsi_produk` varchar(5000) NOT NULL,
+  `durasi_iklan` int(11) NOT NULL,
+  `status_show` int(11) NOT NULL,
+  `gambar1` varchar(500) NOT NULL,
+  `gambar2` varchar(500) NOT NULL,
+  `gambar3` varchar(500) NOT NULL,
+  `gambar4` varchar(500) NOT NULL,
+  `gambar5` varchar(500) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `create_by` int(11) NOT NULL,
+  `update_date` datetime NOT NULL,
+  `update_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_role`
+--
+
+CREATE TABLE `tbl_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_role`
+--
+
+INSERT INTO `tbl_role` (`id`, `role`) VALUES
+(1, 'Super Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_sales`
+--
+
+CREATE TABLE `tbl_sales` (
+  `id_sales` int(11) NOT NULL,
+  `nama_sales` varchar(250) NOT NULL,
+  `jenis_kelamin` varchar(500) NOT NULL,
+  `alamat` varchar(500) NOT NULL,
+  `no_hp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(130) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `image` varchar(130) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `tanggal_daftar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama`, `username`, `image`, `password`, `role_id`, `is_active`, `tanggal_daftar`) VALUES
+(1, 'Super Admin', 'superadmin', 'default.png', '$2y$10$wquZq6hOWt8lLjaA6O8dV.YOJgvNcRfa.7BK2EeEmX0SSB3lGK8rC', 1, 1, '2021-01-01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `tbl_pengajuanharga`
+--
+ALTER TABLE `tbl_pengajuanharga`
+  ADD PRIMARY KEY (`id_pengajuanharga`);
+
+--
+-- Indeks untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_sales`
+--
+ALTER TABLE `tbl_sales`
+  ADD PRIMARY KEY (`id_sales`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_pengajuanharga`
+--
+ALTER TABLE `tbl_pengajuanharga`
+  MODIFY `id_pengajuanharga` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_sales`
+--
+ALTER TABLE `tbl_sales`
+  MODIFY `id_sales` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
