8be9d68d99fd1005d2096a9699ce9b635f488e69
anggakp007 <putraangga2810@gmail.com>
2024-03-03 11:04:27 +0700

    update pengajuan harga

--- application/views/pages/admin/pengajuan-harga.php
+++ application/views/pages/admin/pengajuan-harga.php
@@ -14,6 +14,14 @@
                </ol>
             </div>
          </div>
+         <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-6" style="text-align: right;">
+               <?php if ($user->role_id == 3) { ?>
+                  <a class="btn btn-sm" style="background: transparent; border-color: #858796;color:black;text-decoration: none;border:3px solid;font-weight: bold;" class="collapse-item" href="<?php echo base_url() ?>tambahdatapengajuanharga"> <i class="fas fa-fw fa-plus"></i> <?php echo $this->lang->line('add'); ?> Data </a>
+               <?php } ?>
+            </div>
+         </div>
       </div><!-- /.container-fluid -->
    </section>
 
@@ -35,7 +43,7 @@
                               <tr height="20px">
                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;"><?php echo $this->lang->line('number'); ?></th>
                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Nama Produk</th>
-                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Tanggal Pengajuan</th>
+                                 <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Tanggal Pengajuan</th>
                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Nama Sales</th>
                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Nama Customer</th>
                                  <th style=" padding: 0.75rem;vertical-align: top;border-top: 1px solid #e3e6f0;">Harga</th>
@@ -48,35 +56,40 @@
                               foreach ($pengajuan as $data) {
                               ?>
                                  <tr>
-                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $no ?></td>
-                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_produk ?></td>
+                                    <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $no ?></td>
+                                    <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_produk ?></td>
                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= date('j F Y', strtotime($data->tanggal_pengajuan)) ?></td>
-                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_sales ?></td>
-                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_customer ?></td>
-                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= 'Rp ' . number_format($data->harga, 0,',', '.') ?></td>
+                                    <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_sales ?></td>
+                                    <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= $data->nama_customer ?></td>
+                                    <td style="vertical-align: top;border-top: 1px solid #e3e6f0;"><?= 'Rp ' . number_format($data->harga, 0, ',', '.') ?></td>
                                     <td style="vertical-align: top;border-top: 1px solid #e3e6f0;">
-                                        <?php if ($data->status_approve == 1) { ?>
-                                            <a href="<?= base_url('status-pengajuan-tolak/'.$data->id_pengajuanharga); ?>" class="btn btn-sm btn-danger" role="button">
+                                       <?php if ($this->session->userdata('role_id') == 2) { ?>
+                                          <?php if ($data->status_approve == 1) { ?>
+                                             <a href="<?= base_url('status-pengajuan-tolak/' . $data->id_pengajuanharga); ?>" class="btn btn-sm btn-danger" role="button">
                                                 <i class="fas fa-fw fa-ban"></i> Tolak
-                                            </a>
-                                            <a href="<?= base_url('status-pengajuan-terima/'.$data->id_pengajuanharga); ?>" class="btn btn-sm btn-success mr-1" role="button">
+                                             </a>
+                                             <a href="<?= base_url('status-pengajuan-terima/' . $data->id_pengajuanharga); ?>" class="btn btn-sm btn-success mr-1" role="button">
                                                 <i class="fas fa-fw fa-check"></i> Terima
-                                            </a>
-                                        <?php } ?>
-                                        <?php if ($data->status_approve == 2) { ?>
-                                            <a href="#" class="btn btn-sm btn-success mr-1" role="button">
+                                             </a>
+                                          <?php } ?>
+                                          <?php if ($data->status_approve == 2) { ?>
+                                             <a href="#" class="btn btn-sm btn-success mr-1" role="button">
                                                 <i class="fas fa-fw fa-check"></i> Diterima
-                                            </a>
-                                        <?php } ?>
-                                        <?php if ($data->status_approve == 0) { ?>
-                                            <a href="#" class="btn btn-sm btn-danger" role="button">
+                                             </a>
+                                          <?php } ?>
+                                          <?php if ($data->status_approve == 0) { ?>
+                                             <a href="#" class="btn btn-sm btn-danger" role="button">
                                                 <i class="fas fa-fw fa-ban"></i> Ditolak
-                                            </a>
-                                        <?php } ?>
+                                             </a>
+                                          <?php } ?>
+                                       <?php } ?>
+                                       <?php if ($this->session->userdata('role_id') == 3) { ?>
+                                          <a href="<?= base_url('editdatapengajuanharga/' . $data->id_pengajuanharga); ?>" class="btn btn-sm btn-success" style="background: transparent; border-color: #858796;color:black;text-decoration: none;" role="button" title="Ubah"><i class="fas fa-fw fa-pencil-alt"></i> <?php echo $this->lang->line('change'); ?> </a>
+                                       <?php } ?>
                                     </td>
                                  </tr>
                               <?php
-                                  $no++;
+                                 $no++;
                               }
                               ?>
                            </tbody>
