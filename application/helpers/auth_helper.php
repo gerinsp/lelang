5fa8b25eefe21ee577ff6be5719adff24bdfa72f
anggakp007 <putraangga2810@gmail.com>
2024-03-03 11:03:29 +0700

    update

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -2,13 +2,13 @@
 function cekuser()
 {
     $ci = get_instance();
-   
-    if (!$ci->session->userdata('username')){
+
+    if (!$ci->session->userdata('username')) {
         redirect('login');
-    }else{
+    } else {
         $role_id = $ci->session->userdata('role_id');
         $menu = $ci->uri->segment(1);
-        
+
 
         // $ci->db->select('*');
         // $ci->db->where("url = '$menu' or url_1 = '$menu' or url_2 = '$menu' or url_3 = '$menu' or url_4 = '$menu'");
@@ -21,76 +21,11 @@ function cekuser()
         //     if ($userAccess->num_rows()<1) {
         //         redirect("blocked");
         //     }
-        
 
 
-        if ($role_id == 1) {
-            switch ($menu) {
-                case 'admin':
-                    redirect('pimpinan');
-                    break;
-                case 'supplier':
-                    redirect('pimpinan');
-                    break;
-                case 'agen':
-                    redirect('pimpinan');
-                    break;
 
-                default:
-                    # code...
-                    break;
-            }
-        }
-         else if ($role_id == 2) {
-            switch ($menu) {
-                case 'pimpinan':
-                    redirect('blocked');
-                    break;
-                case 'agen':
-                    redirect('admin');
-                    break;
-                default:
-                    # code...
-                    break;
-            }
 
-        }
-        else if ($role_id == 3) {
-            switch ($menu) {
-                case 'pimpinan':
-                    redirect('blocked');
-                    break;
-                case 'admin':
-                    redirect('blocked');
-                    break;
-                case 'agen':
-                    redirect('supplier');
-                    break;
-                default:
-                    # code...
-                    break;
-            }
-
-        }
-        else if ($role_id == 4) {
-            switch ($menu) {
-                case 'pimpinan':
-                    redirect('blocked');
-                    break;
-                case 'admin':
-                    redirect('blocked');
-                    break;
-                case 'supplier':
-                    redirect('blocked');
-                    break;
-                default:
-                    # code...
-                    break;
-            }
-
-        }
     }
-
 }
 
 function check_access($role_id, $menu_id)
@@ -101,7 +36,7 @@ function check_access($role_id, $menu_id)
     $ci->db->where('menu_id', $menu_id);
     $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows()>0) {
+    if ($result->num_rows() > 0) {
         return "checked='checked'";
     }
 }
@@ -114,7 +49,7 @@ function check_buttontambah($role_id, $menu_id)
     $ci->db->where('add_btn = 1');
     $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows()>0) {
+    if ($result->num_rows() > 0) {
         return "checked='checked'";
     }
 }
@@ -128,7 +63,7 @@ function check_buttonedit($role_id, $menu_id)
     $ci->db->where('update_btn = 1');
     $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows()>0) {
+    if ($result->num_rows() > 0) {
         return "checked='checked'";
     }
 }
@@ -141,7 +76,7 @@ function check_buttonhapus($role_id, $menu_id)
     $ci->db->where('delete_btn = 1');
     $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows()>0) {
+    if ($result->num_rows() > 0) {
         return "checked='checked'";
     }
 }
@@ -155,7 +90,7 @@ function check_buttondetail($role_id, $menu_id)
     $ci->db->where('detail_btn = 1');
     $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows()>0) {
+    if ($result->num_rows() > 0) {
         return "checked='checked'";
     }
 }
@@ -170,7 +105,7 @@ function dd($param)
 
 function user_can($halaman)
 {
-    $ci =& get_instance();
+    $ci = &get_instance();
 
     $id_user = $ci->session->userdata('id_user');
 
@@ -193,4 +128,3 @@ function user_can($halaman)
 
     return false;
 }
-
